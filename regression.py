00d5bfdcb345f77539078b62d00f7921557d74dc
rashika-dabas <rashikadabas19@gmail.com>
2024-01-25 04:42:43 -0500

    adding code files

--- regression.py
+++ regression.py
@@ -28,25 +28,28 @@ def fit_regression_model(X, y):
     Train the model for the given number of epochs.
     Hint: use the train_iteration function.
     Hint 2: while woring you can use the print function to print the loss every 1000 epochs.
-    Hint 3: you can use the previos_loss variable to stop the training when the loss is not changing much.
+    Hint 3: you can use the previous_loss variable to stop the training when the loss is not changing much.
     """
-    learning_rate = 0.01 # Pick a better learning rate
-    num_epochs = 100 # Pick a better number of epochs
-    input_features = 0 # extract the number of features from the input `shape` of X
-    output_features = 0 # extract the number of features from the output `shape` of y
-    model = create_linear_regression_model(input_features, output_features)
+    learning_rate = 0.001 # Pick a learning rate
+    num_epochs = 1000 # Pick number of epochs
+    input_features = X.shape[1] # extract the number of features from the input `shape` of X
+    output_features = y.shape[1] # extract the number of features from the output `shape` of y
+    model = create_linear_regression_model(input_features, output_features) # Create the model
     
-    loss_fn = nn.L1Loss() # Use mean squared error loss, like in class
+    loss_fn = nn.MSELoss() # Use mean squared error loss
 
-    optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+    optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate) # Use SGD optimizer
 
-    previos_loss = float("inf")
+    previous_loss = float("inf") # Initialize the previous loss to a very large number
+    tolerance = 0.0001 # Pick a tolerance value to stop the training when the loss is not changing much
 
-    for epoch in range(1, num_epochs):
+    for epoch in range(1, num_epochs + 1):
         loss = train_iteration(X, y, model, loss_fn, optimizer)
-        if False: # Change this condition to stop the training when the loss is not changing much.
+        if abs(previous_loss - loss.item()) < tolerance: # Stop the training when the loss is not changing much
             break
-        previos_loss = loss.item()
-        # This is a good place to print the loss every 1000 epochs.
-    return model, loss
+        previous_loss = loss.item()
+        # Print the loss every 1000 epochs
+        if epoch % 1000 == 0:
+            print(f'Epoch {epoch} | Loss: {loss.item()}')
 
+    return model, loss
--- regression_test.py
+++ regression_test.py
@@ -1,5 +1,4 @@
 import torch
-import pytest
 from regression import fit_regression_model
 
 
